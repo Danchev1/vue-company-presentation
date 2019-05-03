96ae3db8480457c7b8cb367b9a6d8833eae15b1b
yordandanchev <yordangdanchev@gmail.com>
2019-05-03 19:06:24 +0300

    some push

---
+++ slots-refresh/src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="card-title">
+        <slot name="title"></slot>
+      </div>
+      <div class="card-subtitle">
+        <slot name="subtitle"></slot>
+      </div>
+      <div class="card-text">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildComponent',
+};
+</script>
--- slots-refresh/src/views/Home.vue
+++ slots-refresh/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
+  <div class="home container">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr />
+    <div class="row">
+      <div class="col col-12">
+        <ChildComponent>
+          <template v-slot:title>
+            A simple content passed to named slot title!
+          </template>
+          <template v-slot:subtitle>
+            A simple content passed to named slot subtitle!
+          </template>
+          Content for default slot.
+        </ChildComponent>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ChildComponent from '@/components/ChildComponent.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    ChildComponent,
   },
 };
 </script>
